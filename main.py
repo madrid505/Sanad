e2cb8c11283f53f38d510485a986e3b3cf326075
madrid505 <anassleet877@gmail.com>
2026-07-03 21:47:34 +0300

    Change BOT_TOKEN in main.py
    Updated the BOT_TOKEN for the bot configuration.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from admin_monitor import track_admin_activity, get_admin_report, get_detailed_s
 # --- إعدادات البوت الملكي ---
 API_ID = 33183154
 API_HASH = 'ccb195afa05973cf544600ad3c313b84'
-BOT_TOKEN = '8393076766:AAGs2KW-kh1nK1CQPqD4lLic0kQRtyepMyY'
+BOT_TOKEN = '8393076766:AAHYIo1sB1dmprph28C_f1dkzZzfrWITyZ8'
 OWNER_ID = 5010882230
 # --- تحديث قائمة المجموعات المسموحة ---
 ALLOWED_GROUPS = [
