0f9102b7f9df303b310db5399a3acd843f3245b4
madrid505 <anassleet877@gmail.com>
2026-09-08 15:53:26 +0300

    Enhance photo handling to extract real file ID
    Updated the catch_photo function to extract the real file ID from the photo message and handle exceptions.

--- Extractor.py
+++ Extractor.py
@@ -2,13 +2,19 @@ from telethon import events
 
 
 def setup_extractor_handlers(client, owner_id):
+
   @client.on(events.NewMessage(incoming=True))
   async def catch_photo(event):
     # التحقق من أن الرسالة في المحادثة الخاصة وأن المرسل هو المالك
     if event.is_private and event.sender_id == owner_id:
       if event.photo:
-        p_id = event.photo.id
-        await event.reply(
-            f"<b>✅ تم الاصطياد:</b>\n<code>{p_id}=الجواب_هنا</code>",
-            parse_mode="html",
-        )
+        try:
+          # استخراج الـ File ID الحقيقي من رسالة الصورة
+          real_file_id = event.message.file.id
+          await event.reply(
+              f"<b>✅ تم اصطياد الـ File ID الحقيقي:</b>\n"
+              f"<code>{real_file_id}</code>",
+              parse_mode="html",
+          )
+        except Exception as e:
+          await event.reply(f"❌ حدث خطأ أثناء استخراج المعرف: {e}")
